de94a3debc601089172a0e87b334de62552ba800
daniel sousa <daniel_sousa.unb@outlook.com>
2022-12-21 11:52:38 -0300

    feat: add income arquive

---
+++ application/Income.java
@@ -0,0 +1,17 @@
+package aplicacao;
+public class Income {
+
+	String description;
+	float value;
+	
+	public Income(String description, float value) {
+		this.description = description;
+		this.value = value;
+	}
+
+
+	public float getValue() {
+		return value;
+	}
+
+}
